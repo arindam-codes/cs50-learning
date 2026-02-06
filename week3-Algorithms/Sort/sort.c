4c8896ea3cc39bc5a17f3ef249bb6f9e16556f3a
Arindam B. <arindam.bhattacharjee.codes@gmail.com>
2026-02-06 09:32:21 +0530

    Create sort.c

---
+++ week3-Algorithms/Sort/sort.c
@@ -0,0 +1,12 @@
+sort1 uses: bubble sort
+
+How do you know?: because it takes lower time than sort3 bcz of having omega n
+
+sort2 uses: Merge sort
+
+How do you know?: in random txt file in takes the least time
+
+sort3 uses: selection sort
+
+How do you know?: because it takes more time than sort 1 because of having omega n**2
+
